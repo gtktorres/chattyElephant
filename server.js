ae648f12ff5c5573024279b7012d9751a9199d88
Guevara Torres <guevara.torres619@outlook.com>
2019-12-02 01:12:07 -0500

    initial translate append

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config({ path: '.env' });
     const bodyParser = require('body-parser');
     const cors = require('cors');
     const Chatkit = require('@pusher/chatkit-server');
+    const AWS = require('aws-sdk');
 
     const app = express();
 
@@ -12,9 +13,32 @@ require('dotenv').config({ path: '.env' });
       key: process.env.CHATKIT_SECRET_KEY,
     });
 
+    const translate = new AWS.Translate({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: 'us-east-2',
+    });
+
     app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.post('/translate', (req, res) => {
+      const { text, lang } = req.body;
+      const params = {
+        SourceLanguageCode: 'auto',
+        TargetLanguageCode: lang,
+        Text: text,
+      };
+
+      translate.translateText(params, (err, data) => {
+        if (err) {
+          return res.send(err);
+        };
+
+        res.json(data);
+      });
+    });
 
     app.post('/users', (req, res) => {
       const { userId } = req.body;
